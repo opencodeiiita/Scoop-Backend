a74aab5f8f5dab8c6f1ee411f54cfc0f7694e7cf
Harshal Gainer <harshalgainer5167@gmail.com>
2023-12-16 00:53:30 +0530

    Created validateToken in auth middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,7 @@
-import user from '../models/user.model.js';
+import user from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const verifyToken = util.promisify(jwt.verify);
 
 // next() is the next middleware to be executed
 const isAdmin = async (req, res, next) => {
@@ -12,12 +15,35 @@ const isAdmin = async (req, res, next) => {
     } else {
       return res
         .status(403)
-        .json({ success: false, message: 'User does not have Admin status' });
+        .json({ success: false, message: "User does not have Admin status" });
     }
   } catch (error) {
-    console.error('Error checking admin status:', error);
-    return res.status(500).json({ success: false, message: 'Internal Server Error' });
+    console.error("Error checking admin status:", error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error" });
   }
 };
 
-export default isAdmin;
+const validateToken = async (req, res, next) => {
+  let token
+  let authHeader = req.headers.Authorization || req.headers.authorization
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1]
+    try {
+      const decoded = await verifyToken(token, process.env.ACCESS_TOKEN_SECRET)
+      req.user = decoded.user
+      next();
+    } catch (err) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Not an authorized user" })
+    }
+  } else {
+    return res
+        .status(500)
+        .json({ success: false, message: "Not an authorized user or token is missing" })
+  }
+}
+
+export default { isAdmin, validateToken };
