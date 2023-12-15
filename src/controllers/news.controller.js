ce909020b2d08bd07f9f89174dc3c9ff4ff67166
Shubham Vashishtha <shubhvash87@gmail.com>
2023-12-15 22:25:19 +0530

    Issue: #6 | added second route credible news

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,5 @@
 import News from "../models/news.model.js";
+import User from "../models/user.model.js";
 
 export function topNews(req, res) {
   return News.find({})
@@ -9,4 +10,46 @@ export function topNews(req, res) {
     .catch(() => {
       res.status(500).send({ error: "Internal Server Error" });
     });
+}
+
+export function credibleNews(req, res) {
+  return User.find({})
+    .then((users) => {
+      users.sort(
+        (user1, user2) => user2.myUpvotes.length - user1.myUpvotes.length,
+      );
+      const getAllUsersNews = users.map((user) => getNewsOfUser(user.News));
+      Promise.all(getAllUsersNews)
+        .then((usersNews) => {
+          const credibleNews = usersNews.reduce(
+            (acc, news) => acc.concat(news),
+            [],
+          );
+          res.status(200).send(credibleNews);
+        })
+        .catch((error) => {
+          res
+            .status(500)
+            .send({ error: "Internal Server Error", details: error.message });
+        });
+    })
+    .catch(() => {
+      res.status(500).send({ error: "Internal Server Error" });
+    });
+}
+
+function getNewsOfUser(newsIds) {
+  return new Promise((resolve, reject) => {
+    News.find({
+      _id: {
+        $in: newsIds,
+      },
+    })
+      .then((news) => {
+        resolve(news);
+      })
+      .catch(() => {
+        reject();
+      });
+  });
 }
