8d7dbde4b26eb28e5825fc39b48a86dafffe6a78
Shubham Vashishtha <shubhvash87@gmail.com>
2023-12-15 22:25:19 +0530

    Issue: #6 | added second route credible news

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
-import { Schema, model } from "mongoose";
+import pkg from "mongoose";
+const { Schema, model } = pkg;
 
 const userSchema = new Schema(
   {
--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { topNews } from "../controllers/news.controller.js";
+import { topNews, credibleNews } from "../controllers/news.controller.js";
 
 router.get("/top", [], topNews);
+router.get("/credible", [], credibleNews);
 
 export default router;
