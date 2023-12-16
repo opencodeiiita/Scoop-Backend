8511c34f52b581f5070d19a93ee0963bddb7185d
Shubham Vashishtha <shubhvash87@gmail.com>
2023-12-16 12:11:43 +0530

    Issue: #6 | resolved comments 2

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -2,43 +2,38 @@ import News from "../models/news.model.js";
 import User from "../models/user.model.js";
 
 export async function topNews(req, res) {
-  return News.find({})
-    .then((news) => {
-      news.sort((n1, n2) => n2.UpvoteCount - n1.UpvoteCount);
-      res.status(200).send(news);
-    })
-    .catch(() => {
-      res.status(500).send({ error: "Internal Server Error" });
-    });
+  try {
+    const news = await News.find({}).sort({ UpvoteCount: -1 });
+    res.status(200).send(news);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
 }
 
 export async function credibleNews(req, res) {
-  return User.find({})
-    .then((users) => {
-      users.sort(
-        (user1, user2) => user2.myUpvotes.length - user1.myUpvotes.length,
-      );
-      const getAllUsersNews = users.map((user) => getNewsOfUser(user.News));
-      Promise.all(getAllUsersNews)
-        .then((usersNews) => {
-          const credibleNews = usersNews.reduce(
-            (acc, news) => acc.concat(news),
-            [],
-          );
-          res.status(200).send(credibleNews);
-        })
-        .catch((error) => {
-          res
-            .status(500)
-            .send({ error: "Internal Server Error", details: error.message });
-        });
-    })
-    .catch(() => {
-      res.status(500).send({ error: "Internal Server Error" });
-    });
+  try {
+    const users = await User.find({}).sort({ myUpvotes: -1 });
+    const usersNews = users.map((user) => getNewsOfUser(user.News));
+    Promise.all(usersNews)
+      .then((usersNews) => {
+        const credibleNews = usersNews.reduce(
+          (acc, news) => acc.concat(news),
+          [],
+        );
+        res.status(200).send(credibleNews);
+      })
+      .catch((error) => {
+        res
+          .status(500)
+          .send({ error: "Internal Server Error", details: error.message });
+      });
+  } catch (err) {
+    res.status(500).send({ error: "Internal Server Error" });
+  }
 }
 
-async function getNewsOfUser(newsIds) {
+function getNewsOfUser(newsIds) {
   return new Promise((resolve, reject) => {
     News.find({
       _id: {
