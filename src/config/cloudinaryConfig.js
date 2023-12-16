bf3fc04c15ee5d57d8dbed01e3609f30ea1b72f2
Rohit Pandey <120464365+Rohit-110@users.noreply.github.com>
2023-12-16 16:02:58 +0530

    Create cloudinaryConfig.js

---
+++ src/config/cloudinaryConfig.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: 'your_cloud_name',
+  api_key: 'your_api_key',
+  api_secret: 'your_api_secret',
+});
+
+module.exports = cloudinary;
