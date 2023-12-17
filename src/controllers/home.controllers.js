e04292e513f25a055572d60a2c17dd021511013f
Dikshant <dikkpsd@gmail.com>
2023-12-17 15:32:35 +0530

    Tested credible News

--- src/controllers/home.controllers.js
+++ src/controllers/home.controllers.js
@@ -2,6 +2,7 @@ import News from "../models/news.model.js";
 import User from "../models/user.model.js";
 import {
   response_200,
+  response_204,
   response_500
 } from "../utils/responseCodes.js";
 
@@ -10,7 +11,7 @@ export async function latestnews(req, res) {
     try {
       let news = await News.find().select("-Upvotes").sort({ createdAt: -1 }).limit(5);
   
-      if(news.length == 0) throw new Error('No News to Fetch');
+      if(news.length == 0) return response_204(res, "News Not Found");
 
       // res.status(200).json({ news });
       return response_200(res, "Latest News", { news });
@@ -25,7 +26,7 @@ export async function latestnews(req, res) {
     try {
       let news = await News.find({}).select("-Upvotes").sort({ UpvoteCount: -1 }).limit(5);
 
-      if(news.length == 0) throw new Error('No News to Fetch');
+      if(news.length == 0) return response_204(res, "News Not Found");
         
       return response_200(res, "Top News", {news});
     } catch (error) {
@@ -38,16 +39,16 @@ export async function latestnews(req, res) {
     try {
       const users = await User.find({News: {$exists: true, $ne: []}}).sort({ myUpvotes: -1 }).limit(5);
   
-      if(users.length == 0) throw new Error('No Users found who have posted news');
-  
-      let usersNews = users.map(async (user) => 
+      if(users.length == 0) return response_204(res, "No Users Found who have posted news");
+      let usersNewsPromises = users.map(async (user) => 
       await News.find({_id : {$in: user.News}})
       .select("-Upvotes")
       .sort({createdAt: -1})
       .limit(1));
        
-    usersNews = usersNews.map((news) => {delete news.Upvotes; return news; })
 
+      const usersNews = await Promise.all(usersNewsPromises);
+        
       return response_200(res, 'Credible News', {usersNews});
   
       
@@ -63,10 +64,10 @@ export async function latestnews(req, res) {
       const news_tags = await News.distinct("Tags");
 
 
-      if(news_tags.length == 0)
-        throw new Error("No News Tags Found");
+      if(news_tags.length == 0) return response_204(res, "No News Categories Found");
+
       return response_200(res, "News Categories", {news_tags});
-      
+
     }
     catch(err)
     {
