15c4dd669378eb9919e342d7147a4ad28e12b69c
Rohit Pandey <120464365+Rohit-110@users.noreply.github.com>
2023-12-18 12:27:19 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -32,6 +32,9 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    fileUrl: {
+    type: String, 
+  },
     isAdmin: {
       type: Boolean,
       default: false,
